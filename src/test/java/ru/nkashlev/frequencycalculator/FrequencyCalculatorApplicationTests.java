1c77b7a702e96a860a158049aeadaaba4327ad04
nkashlev <kashlev-1996@mail.ru>
2024-01-21 22:49:33 +0300

    fixup test

---
+++ src/test/java/ru/nkashlev/frequencycalculator/FrequencyCalculatorApplicationTests.java
@@ -0,0 +1,14 @@
+package ru.nkashlev.frequencycalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrequencyCalculatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		FrequencyCalculatorApplication.main(new String[]{});
+	}
+
+}
