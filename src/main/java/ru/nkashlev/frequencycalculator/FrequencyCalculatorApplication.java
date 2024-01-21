fe09cac63734b5e08845c1a689e02d3a9a928538
nkashlev <kashlev-1996@mail.ru>
2024-01-21 13:40:28 +0300

    Initial commit

---
+++ src/main/java/ru/nkashlev/frequencycalculator/FrequencyCalculatorApplication.java
@@ -0,0 +1,13 @@
+package ru.nkashlev.frequencycalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrequencyCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrequencyCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/nkashlev/frequencycalculator/FrequencyCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nkashlev.frequencycalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrequencyCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
